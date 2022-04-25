1d29da07862157fb8c24188ffefc434fbed170f7
Squantor <squantmuts@gmail.com>
2022-04-25 22:09:05 +0200

    got USART output working correctly, ported over SSD1306 demo

--- software/embedded_ui_mini/src/nuclone_mini_ui.cpp
+++ software/embedded_ui_mini/src/nuclone_mini_ui.cpp
@@ -20,14 +20,16 @@ void crudeDelay(uint32_t iterations) {
 }
 
 void boardInit(void) {
-  sysconEnableClocks(SYSCON, CLKCTRL_SWM | CLKCTRL_IOCON | CLKCTRL_GPIO | CLKCTRL_I2C0);
+  sysconEnableClocks(SYSCON, CLKCTRL_SWM | CLKCTRL_IOCON | CLKCTRL_GPIO | CLKCTRL_UART0 | CLKCTRL_I2C0);
 
   ioconSetupPin(IOCON, IOCON_XTAL_IN, IOCON_MODE(IOCON_MODE_INACTIVE));
   ioconSetupPin(IOCON, IOCON_XTAL_OUT, IOCON_MODE(IOCON_MODE_INACTIVE));
   ioconSetupPin(IOCON, IOCON_UART_RX, IOCON_MODE(IOCON_MODE_PULLUP));
   ioconSetupPin(IOCON, IOCON_UART_TX, IOCON_MODE(IOCON_MODE_INACTIVE));
+  ioconSetupPin(IOCON, IOCON_I2C_SCL, IOCON_I2CMODE(IOCON_I2CMODE_STD));
+  ioconSetupPin(IOCON, IOCON_I2C_SDA, IOCON_I2CMODE(IOCON_I2CMODE_STD));
 
-  swmEnableFixedPin(SWM, SWM_EN0_XTALIN | SWM_EN0_XTALOUT);
+  swmEnableFixedPin(SWM, SWM_EN0_XTALIN | SWM_EN0_XTALOUT | SWM_EN0_I2C0_SDA | SWM_EN0_I2C0_SCL);
   SwmMovablePinAssign(SWM, SWM_USART0_TXD_O, SWM_UART_TX);
   SwmMovablePinAssign(SWM, SWM_USART0_RXD_I, SWM_UART_RX);
   sysconEnableClocks(SYSCON, CLKCTRL_SWM);
@@ -47,8 +49,15 @@ void boardInit(void) {
   sysconMainClockSelect(SYSCON, MAINCLKSEL_PLL_OUT);
 
   // setup debug output uart
-  sysconUartClockDiv(SYSCON, UART_DEBUG_SPEED);
-  usartSetBaud(USART0, CLOCK_AHB, 9600);
+  sysconUartClockDiv(SYSCON, 2);
+  usartSetBaud(USART0, CLOCK_AHB, UART_DEBUG_SPEED);
   usartSetConfig(UART_DEBUG, DATALEN_8, PARITY_NONE, STOPLEN_1, 0);
   usartTXEnable(UART_DEBUG);
+
+  // setup i2c
+  i2cSetClockDivider(I2C0, 3);
+  i2cSetConfiguration(I2C0, I2C_CFG_MSTEN);
+
+  // setup systick
+  SysTick_Config(CLOCK_AHB / TICKS_PER_S);
 }
